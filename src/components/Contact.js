48951f5f2a231bee44827516d01897797f9dcabb
alaihsan <iksan.qren@gmail.com>
2020-10-11 19:37:42 +0700

    update contact #1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = (props) => (
             <section>
                 <form method="post" action="#">
                     <div className="field half first">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name">Nama</label>
                         <input type="text" name="name" id="name" />
                     </div>
                     <div className="field half">
@@ -14,7 +14,7 @@ const Contact = (props) => (
                         <input type="text" name="email" id="email" />
                     </div>
                     <div className="field">
-                        <label htmlFor="message">Message</label>
+                        <label htmlFor="message">Pesan</label>
                         <textarea name="message" id="message" rows="6"></textarea>
                     </div>
                     <ul className="actions">
