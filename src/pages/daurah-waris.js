417fcd9c0817ce6fd99b261345a45446b44e708c
alaihsan <iksan.qren@gmail.com>
2020-10-15 19:01:45 +0700

    add page sahil, tca, daurah waris #3

---
+++ src/pages/daurah-waris.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const DaurahWaris = () => {
+    return (
+        <div>
+            <Link to="/">
+            <h1>
+                Daurah Waris
+            </h1>
+            </Link>
+
+            <h2>
+                ini adalah halaman untuk Daurah Waris
+            </h2>
+        </div>
+    )
+}
+
+export default DaurahWaris
---
+++ src/pages/tca.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const tca = () => {
+    return (
+        <div>
+            <Link to="/tca">
+            <h1>
+                Tahfidz Center Asshaff
+            </h1>
+            </Link>
+
+            <h2>
+                ini adalah halaman khusus TCA
+            </h2>
+        </div>
+    )
+}
+
+export default tca
