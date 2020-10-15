dfebdc2d8a627776eda20ba8fcf273ffa7891a61
alaihsan <iksan.qren@gmail.com>
2020-10-15 20:32:04 +0700

    add page : galeri, pengajian-rutin, santunan-yatim, unit-bisnis #4

---
+++ src/pages/santunan-yatim.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const SantunanYatim = () => {
+    return (
+        <div>
+        <Link to="santunan-yatim"><h1>Santunan yatim</h1></Link>
+        <h2>ini adalah halaman santunan yatim</h2>
+        </div>
+    )
+}
+
+export default SantunanYatim
---
+++ src/pages/unit-bisnis.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const UnitBisnis = () => {
+    return (
+        <div>
+            <h1>Unit Bisnis</h1>
+            <h2>ini adalah halaman untuk bisnis</h2>
+        </div>
+    )
+}
+
+export default UnitBisnis
