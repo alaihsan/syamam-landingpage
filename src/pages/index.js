25d59128fcee4b9c637ec3fb6b70fd7402dc2380
alaihsan <iksan.qren@gmail.com>
2021-05-13 20:50:53 +0700

    add navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
+import {Link} from 'gatsby'
+import Header from "../components/header"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <div>
+    <Header></Header>
+  </div>
+);
